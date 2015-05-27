1e39f8d47ba35a0780406b4d4d46643b05924234
Ashish Tyagi <ashisht@thoughtworks.com>
2015-05-27 22:03:44 +0530

    Initial Skeleton With Factory

---
+++ Apple Store/Product.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class Product {
+    
+    private let fullImageName : String
+    private let thumbnailImageName : String
+    
+    let name : String
+    let price : String
+    let detailText : String
+    
+    var fullImage : UIImage? { get{
+        return UIImage(named: fullImageName)
+        }
+    }
+    
+    var thumbnailImage : UIImage? { get{
+        return UIImage(named: thumbnailImageName)
+        }
+    }
+    
+    init(name: String, price: String, detailText: String,thumbnailImageName tName: String, fullImageName fName: String){
+        self.name = name
+        self.price = price
+        self.detailText = detailText
+        fullImageName = fName
+        thumbnailImageName = tName
+    }
+}
---
+++ Apple Store/ProductFactory.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ProductFactory: NSObject {
+    
+    class func getProducts () -> [Product]{
+        let jsonData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("apple-products", ofType: "json")!)
+        
+        var error: NSError?
+        let productsDict = NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.allZeros, error: &error) as! Dictionary<String, AnyObject>
+        
+        let productsArray = productsDict["products"] as! Array<Dictionary<String, String>>
+        
+        return productsArray.map {aProductDict in
+            Product(name: aProductDict["title"]!,
+            price: aProductDict["price"]!,
+            detailText: aProductDict["detail"]!,
+            thumbnailImageName: aProductDict["thumbnailImage"]!,
+            fullImageName: aProductDict["fullImage"]!)
+        }
+    }
+   
+}
